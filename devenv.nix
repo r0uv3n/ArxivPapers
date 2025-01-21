612d6d3bfec3c9e18d2045763625dc65b1d8fcbd
Henry Ruben Fischer <henryrubenfischer@gmail.com>
2025-01-21 17:38:24 +0100

    Expand devenv setup

--- devenv.nix
+++ devenv.nix
@@ -1,52 +1,67 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  config = {
+    # https://devenv.sh/basics/
+    env.GREET = "devenv";
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+    # https://devenv.sh/packages/
+    packages = [ pkgs.git ];
 
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
+    # https://devenv.sh/languages/
+    # languages.rust.enable = true;
 
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
+    # https://devenv.sh/processes/
+    # processes.cargo-watch.exec = "cargo-watch";
 
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
+    # https://devenv.sh/services/
+    # services.postgres.enable = true;
 
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
+    # https://devenv.sh/scripts/
+    scripts.hello.exec = ''
+      echo hello from $GREET
+    '';
 
-  enterShell = ''
-    hello
-    git --version
-  '';
+    enterShell = ''
+      hello
+      git --version
+    '';
 
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
+    # https://devenv.sh/tasks/
+    # tasks = {
+    #   "myproj:setup".exec = "mytool build";
+    #   "devenv:enterShell".after = [ "myproj:setup" ];
+    # };
 
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
+    # https://devenv.sh/tests/
+    enterTest = ''
+      echo "Running tests"
+      git --version | grep --color=auto "${pkgs.git.version}"
+    '';
 
-  languages.python = {
-    enable = true;
-    venv.enable = true;
-    venv.requirements = ./requirements.txt;
+    languages.python = {
+      enable = true;
+      venv.enable = true;
+      venv.requirements = ./requirements.txt;
+    };
+
+    # https://devenv.sh/pre-commit-hooks/
+    # pre-commit.hooks.shellcheck.enable = true;
+
+    # See full reference at https://devenv.sh/reference/options/
   };
 
 
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
+  options = {
+    arxiv-papers.package = pkgs.lib.mkOption {
+      type = config.lib.types.outputOf lib.types.package;
+      description = "Create audio podcasts from arxiv papers";
+      default = pkgs.buildPythonPackage {
+        format = "pyproject";
+      };
 
-  # See full reference at https://devenv.sh/reference/options/
+      defaultText = "ArxivPapers";
+    };
+  };
+
 }
